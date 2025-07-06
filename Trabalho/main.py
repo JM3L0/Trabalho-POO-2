27f5647b423b8144dd498e59f236276002f83de1
JM3L0 <jsousarufinoleal@gmail.com>
2025-07-06 20:47:35 -0300

    muitas coisas

--- Trabalho/main.py
+++ Trabalho/main.py
@@ -39,10 +39,10 @@ def menu_principal():
         opcao = input("Escolha uma opção: ")
 
         if opcao == "1":
-            senha = input("Digite a senha do gerente: ")
             
-            if util.verifica_tamanho_senha(senha) == False:
-                continue
+            print("\n=== Login como Gerente ===")
+            senha = util.capturar_senha()
+            
             if sistema.login(gerente, senha):
                 print("\nLogin realizado com sucesso como Gerente!")
                 while True:
@@ -50,21 +50,23 @@ def menu_principal():
                     opcao_gerente = input("Escolha uma opção: ")
 
                     if opcao_gerente == "1":
+                        
                         nome = input("\nNome do funcionário: ")
-                        cpf = input("CPF do funcionário: ")
-                        senha = input("Senha do funcionário: ")
+                        cpf = util.captura_cpf()
+                        senha = util.capturar_senha()
                         
-                        if util.verifica_tamanho_senha(senha) == False:
-                            continue
                         gerente.adicionar_funcionario(nome, cpf, senha)
-                    elif opcao_gerente == "2":
-                        cpf = input("\nCPF do funcionário a ser removido: ")
                         
+                    elif opcao_gerente == "2":
+                        cpf = util.captura_cpf()
                         gerente.remover_funcionario(cpf)
+                        
                     elif opcao_gerente == "3":
                         gerente.listar_funcionarios()
+                        
                     elif opcao_gerente == "4":
                         gerente.imprimir_historico_gerente()
+                        
                     elif opcao_gerente == "0":
                         print("\nLogout do Gerente realizado.")
                         break
@@ -72,17 +74,14 @@ def menu_principal():
                         print("\nOpção inválida! Tente novamente.")
             else:
                 print("\nSenha incorreta!")
+                
         elif opcao == "2":
-            cpf = input("\nDigite o CPF do funcionário: ")
-            senha = input("Digite a senha do funcionário: ")
+            cpf = util.captura_cpf()
+            senha = util.capturar_senha()
             
-            if util.verifica_tamanho_senha(senha) == False:
-                continue
-            funcionario = None
-            for f in gerente.funcionarios.values():
-                if f.cpf == cpf:
-                    funcionario = f
-                    break
+            print("\n=== Login como Funcionário ===")
+            funcionario = util.existe_funcionario(gerente.funcionarios, cpf)
+            
             if funcionario and sistema.login(funcionario, senha):
                 print(f"\nLogin realizado com sucesso como {funcionario.nome}!")
                 while True:
@@ -92,7 +91,7 @@ def menu_principal():
                     if opcao_funcionario == "1":
                         try:
                             nome = input("\nNome do hóspede: ")
-                            cpf = input("CPF do hóspede: ")
+                            cpf = util.captura_cpf()
                             
                             try:
                                 quarto = int(input("Número do quarto (1 a 10): "))
@@ -107,14 +106,17 @@ def menu_principal():
                             funcionario.registrar_hospede(checkin, hospede)
                         except ValueError:
                             print("\nEntrada inválida. O número do quarto deve ser um número inteiro.")
+                            
                     elif opcao_funcionario == "2":
                         funcionario.listar_hospedes(checkin)
+                        
                     elif opcao_funcionario == "3":
                         funcionario.listar_quartos_disponiveis(checkin)
-                    elif opcao_funcionario == "4":
-                        cpf = input("CPF do hóspede a ser removido: ")
                         
+                    elif opcao_funcionario == "4":
+                        cpf = util.captura_cpf()
                         checkout.remover_hospede(cpf, checkin, funcionario, nome)
+                        
                     elif opcao_funcionario == "5":
                         print(f"\nHistórico de ações do funcionário {funcionario.nome}")
                         if not funcionario.historico:
@@ -122,6 +124,7 @@ def menu_principal():
                         else:
                             for acao in funcionario.historico:
                                 print(acao)
+                                
                     elif opcao_funcionario == "0":
                         print("\nLogout do Funcionário realizado.")
                         break
--- Trabalho/util.py
+++ Trabalho/util.py
@@ -1,10 +1,36 @@
 def ordenar_quartos(lista):
     return sorted(lista)
 
-def verifica_tamanho_senha(senha):
-    if len(senha) < 3:
-        print("\nSenha muito curta. A senha deve ter no mínimo 3 caracteres.")
-        return False
-    else:
-        return True
     
+def captura_cpf():
+    
+    cpf = input("Digite o CPF (somente números): ")
+    
+    while not (cpf.isdigit() and len(cpf) == 11):
+        print("\nCPF inválido. Deve ser um número de 11 caracteres.")
+        cpf = input("Digite o CPF (somente números): ")
+        
+    return cpf  # Retorna o CPF válido como string
+
+def imprimir_cpf(cpf):
+    if cpf.isdigit() and len(cpf) == 11:
+        print(f"\nCPF: {cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}")
+    else:
+        print("\nCPF inválido.")
+        
+def capturar_senha():
+    
+    senha = input("Digite a senha (mínimo 3 caracteres): ")
+    
+    while len(senha) < 3:
+        print("\nSenha inválida. Deve ter pelo menos 3 caracteres.")
+        senha = input("Digite a senha (mínimo 3 caracteres): ")
+        
+    return senha  # Retorna a senha válida
+
+def existe_funcionario(funcionarios, cpf):
+    
+    if cpf in funcionarios:
+        return funcionarios[cpf]
+
+    return None  # Retorna None se o funcionário não existir
